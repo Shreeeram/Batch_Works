2773689b858e6d8a75a8b9f92584dd627b99646f
Shreeram Vaidhyanathan <iamshreeram@users.noreply.github.com>
2017-10-28 10:08:57 -0600

    Add files via upload

---
+++ passcode_generator.bat
@@ -0,0 +1,55 @@
+:: This is a passcode generator
+:: passcode_generator - creates random passcode based on our input choice
+
+Code:
+
+@echo off
+:Start2
+cls
+goto Start
+:Start
+title Passcode Generator
+echo I will make you a new passcode.
+echo Please write the passcode down somewhere in case you forget it.
+echo ----------------------------------------­-----------------------
+echo 1) 1 Random Passcode
+echo 2) 5 Random passcodes
+echo 3) 10 Random passcodes
+echo Input your choice
+set input=
+set /p input= Choice:
+if %input%==1 goto A if NOT goto Start2
+if %input%==2 goto B if NOT goto Start2
+if %input%==3 goto C if NOT goto Start2
+:A
+cls
+echo Your passcode is %random%
+echo Now choose what you want to do.
+echo 1) Go back to the beginning
+echo 2) Exit
+set input=
+set /p input= Choice:
+if %input%==1 goto Start2 if NOT goto Start 2
+if %input%==2 goto Exit if NOT goto Start 2
+:Exit
+exit
+:B
+cls
+echo Your 5 passcodes are %random%, %random%, %random%, %random%, %random%.
+echo Now choose what you want to do.
+echo 1) Go back to the beginning
+echo 2) Exit
+set input=
+set /p input= Choice:
+if %input%==1 goto Start2 if NOT goto Start 2
+if %input%==2 goto Exit if NOT goto Start 2
+:C
+cls
+echo Your 10 passcodes are %random%, %random%, %random%, %random%, %random%, %random%, %random%, %random%, %random%, %random%
+echo Now choose what you want to do.
+echo 1) Go back to the beginning
+echo 2) Exit
+set input=
+set /p input= Choice:
+if %input%==1 goto Start2 if NOT goto Start 2
+if %input%==2 goto Exit if NOT goto Start 2
